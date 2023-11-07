1f7e3e93d846533e740cab0d573225f99893745a
maxguuse <max.pink.goose@gmail.com>
2023-11-08 02:59:26 +0300

    refactored discord part of poll start command

--- apps/bot/internal/interactions/polls/start_command.go
+++ apps/bot/internal/interactions/polls/start_command.go
@@ -6,7 +6,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/maxguuse/birdcord/apps/bot/internal/embeds"
 	"github.com/maxguuse/birdcord/libs/grpc/generated/polls"
-	"strings"
+	"github.com/samber/lo"
 )
 
 var start = &discordgo.ApplicationCommandOption{
@@ -34,21 +34,50 @@ func (p *Polls) handleStart(
 	s *discordgo.Session, i *discordgo.Interaction,
 	options map[string]*discordgo.ApplicationCommandInteractionDataOption,
 ) {
-	pollOptions := strings.Split(options["options"].StringValue(), "|")
-	if len(pollOptions) > 25 {
-		processInvalidOptionsCount(s, i)
-		return
-	}
-
-	// Send message with poll Embed
-	message, err := s.ChannelMessageSendComplex(i.ChannelID, &discordgo.MessageSend{
-		Content: "Опрос формируется...",
-	})
+	// Send message
+	message, err := s.ChannelMessageSend(i.ChannelID, "Опрос формируется...")
 	if err != nil {
 		fmt.Println("Error sending poll message:", err) //TODO Replace with logger
 		return
 	}
-
+	// Process poll data
+	response, err := p.client.CreatePoll(
+		context.Background(),
+		&polls.CreatePollRequest{
+			Title:           options["title"].StringValue(),
+			Options:         options["options"].StringValue(),
+			DiscordId:       message.ID,
+			ChannelId:       message.ChannelID,
+			DiscordAuthorId: i.Member.User.ID,
+			DiscordGuildId:  i.GuildID,
+		},
+	)
+	if err != nil {
+		fmt.Println("Error from gRPC CreatePoll:", err) //TODO Replace with logger
+		processPollFailure(s, i, message, err)
+		return
+	}
+	// Edit poll message with actual data
+	components := generatePollComponents(response)
+	_, err = s.ChannelMessageEditComplex(&discordgo.MessageEdit{
+		Channel: message.ChannelID,
+		ID:      message.ID,
+		Content: new(string),
+		Embed: embeds.ActivePoll(
+			options["title"].StringValue(),
+			components.description,
+			fmt.Sprintf("Poll ID: %d", response.PollId),
+			0,
+			i.Member.Nick,
+			i.Member.AvatarURL("1024"),
+		),
+		Components: components.actionRows,
+	})
+	if err != nil {
+		fmt.Println("Error editing poll message:", err) //TODO Replace with logger
+		processPollFailure(s, i, message, err)
+		return
+	}
 	// Send interaction response if poll message sent successfully
 	err = s.InteractionRespond(i, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -59,100 +88,77 @@ func (p *Polls) handleStart(
 	})
 	if err != nil {
 		fmt.Println("Error responding to poll:", err) //TODO Replace with logger
-		return
-	}
-
-	// Create record in DB
-	createPollResponse, err := p.client.CreatePoll(
-		context.Background(),
-		&polls.CreatePollRequest{
-			Title:           options["title"].StringValue(),
-			Options:         pollOptions,
-			DiscordId:       message.ID,
-			ChannelId:       message.ChannelID,
-			DiscordAuthorId: i.Member.User.ID,
-			DiscordGuildId:  i.GuildID,
-		},
-	)
-	if err != nil {
-		fmt.Println("Error creating poll:", err) //TODO Replace with logger
-		processFailPoll(s, i, message)
-		return
-	}
-
-	description, buttons := generateButtonsAndDescription(pollOptions, createPollResponse)
-
-	actionRows := generateActionRows(buttons)
-
-	_, err = s.ChannelMessageEditComplex(&discordgo.MessageEdit{
-		Channel: message.ChannelID,
-		ID:      message.ID,
-		Content: new(string),
-		Embed: embeds.ActivePoll(
-			options["title"].StringValue(),
-			description,
-			fmt.Sprintf("Poll ID: %d", createPollResponse.PollId),
-			0,
-			i.Member.Nick,
-			i.Member.AvatarURL("1024"),
-		),
-		Components: actionRows,
-	})
-	if err != nil {
-		fmt.Println("Error editing poll message:", err) //TODO Replace with logger
-		processFailPoll(s, i, message)
-		return
+		processPollFailure(s, i, message, err)
 	}
 }
 
-func generateButtonsAndDescription(pollOptions []string, createPollResponse *polls.CreatePollResponse) (string, []discordgo.MessageComponent) {
-	var description string
-	buttons := make([]discordgo.MessageComponent, len(pollOptions))
-	for i, option := range createPollResponse.Options {
+type pollComponents struct {
+	description string
+	actionRows  []discordgo.MessageComponent
+}
+
+func generatePollComponents(response *polls.CreatePollResponse) *pollComponents {
+	description := ""
+	actionRows := make([]*discordgo.ActionsRow, 0, (len(response.Options)+4)/5)
+
+	for i, option := range response.Options {
 		description += fmt.Sprintf("%d) %s > %d \n", i+1, option.Title, option.TotalVotes)
-		buttons[i] = discordgo.Button{
+
+		// If the current index is a multiple of 5, create a new ActionsRow
+		if i%5 == 0 {
+			actionRow := &discordgo.ActionsRow{
+				Components: make([]discordgo.MessageComponent, 0, 5),
+			}
+			actionRows = append(actionRows, actionRow)
+		}
+
+		// Add the current button to the last ActionsRow
+		lastActionRow := actionRows[len(actionRows)-1]
+		lastActionRow.Components = append(lastActionRow.Components, discordgo.Button{
 			Label:    option.Title,
 			Style:    discordgo.PrimaryButton,
-			CustomID: fmt.Sprintf("poll_%d_choice_%d", createPollResponse.PollId, option.Id),
-		}
+			CustomID: option.CustomId,
+		})
 	}
-	return description, buttons
-}
 
-func generateActionRows(buttons []discordgo.MessageComponent) []discordgo.MessageComponent {
-	actionRows := make([]discordgo.MessageComponent, 0, (len(buttons)+4)/5)
-	for i := 0; i < len(buttons); i += 5 {
-		actionRow := discordgo.ActionsRow{}
-		for j := 0; j < 5 && i+j < len(buttons); j++ {
-			actionRow.Components = append(actionRow.Components, buttons[i+j])
-		}
-		actionRows = append(actionRows, &actionRow)
-	}
-	return actionRows
-}
-
-func processInvalidOptionsCount(s *discordgo.Session, i *discordgo.Interaction) {
-	err := s.InteractionRespond(i, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: &discordgo.InteractionResponseData{
-			Flags:   discordgo.MessageFlagsEphemeral,
-			Content: "Слишком много опций.",
-		},
-	})
-	if err != nil {
-		fmt.Println("Error responding to poll:", err) //TODO Replace with logger
+	return &pollComponents{
+		description: description,
+		actionRows: lo.Map(actionRows, func(actionRow *discordgo.ActionsRow, _ int) discordgo.MessageComponent {
+			return actionRow
+		}),
 	}
 }
 
-func processFailPoll(s *discordgo.Session, i *discordgo.Interaction, message *discordgo.Message) {
+func processPollFailure(s *discordgo.Session, i *discordgo.Interaction, message *discordgo.Message, err error) {
 	deleteErr := s.ChannelMessageDelete(message.ChannelID, message.ID)
 	if deleteErr != nil {
 		fmt.Println("Error deleting poll message:", deleteErr) //TODO Replace with logger
 		return
 	}
-	_, followupErr := s.FollowupMessageCreate(i, false, &discordgo.WebhookParams{})
+
+	responseErr := s.InteractionRespond(i, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Flags: discordgo.MessageFlagsEphemeral,
+		},
+	})
+	if responseErr != nil {
+		fmt.Println("Error responding to poll interaction with error:", responseErr) //TODO Replace with logger
+		return
+	}
+
+	_, followupErr := s.FollowupMessageCreate(i, false, &discordgo.WebhookParams{
+		Content: "Ошибка при создании опроса!",
+		Flags:   discordgo.MessageFlagsEphemeral,
+		Embeds: []*discordgo.MessageEmbed{
+			{
+				Title:       "Сообщение об ошибке",
+				Description: err.Error(),
+			},
+		},
+	})
 	if followupErr != nil {
-		fmt.Println("Error sending followup to poll:", followupErr) //TODO Replace with logger
+		fmt.Println("Error sending poll error message:", followupErr) //TODO Replace with logger
 		return
 	}
 }
--- apps/polls/internal/grpc/create_poll.go
+++ apps/polls/internal/grpc/create_poll.go
@@ -2,43 +2,60 @@ package grpc
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/maxguuse/birdcord/libs/grpc/generated/polls"
 	"github.com/maxguuse/birdcord/libs/sqlc/queries"
+	"github.com/samber/lo"
+	"strings"
 )
 
 func (p PollsServer) createPoll(ctx context.Context, request *polls.CreatePollRequest) (*polls.CreatePollResponse, error) {
+	pollOptions := strings.Split(request.Options, "|")
+	if len(pollOptions) < 2 || len(pollOptions) > 25 {
+		return &polls.CreatePollResponse{}, errors.New("количество вариантов ответа должно быть от 2 до 25")
+	}
+
+	countEmptyOptions := lo.CountBy(pollOptions, func(option string) bool {
+		return option == ""
+	})
+	if countEmptyOptions > 0 {
+		return &polls.CreatePollResponse{}, errors.New("варианты ответа не могут быть пустыми")
+	}
+
 	// Create new record in polls table in DB
-	createPollParams := queries.CreatePollParams{}
-	createPollParams.Title = pgtype.Text{
-		String: request.Title,
-		Valid:  true,
-	}
-	createPollParams.DiscordID = pgtype.Text{
-		String: request.DiscordId,
-		Valid:  true,
-	}
-	createPollParams.DiscordAuthorID = pgtype.Text{
-		String: request.DiscordAuthorId,
-		Valid:  true,
-	}
-	createPollParams.DiscordGuildID = pgtype.Text{
-		String: request.DiscordGuildId,
-		Valid:  true,
-	}
-	createPollParams.ChannelID = pgtype.Text{
-		String: request.ChannelId,
-		Valid:  true,
+	createPollParams := queries.CreatePollParams{
+		Title: pgtype.Text{
+			String: request.Title,
+			Valid:  true,
+		},
+		DiscordID: pgtype.Text{
+			String: request.DiscordId,
+			Valid:  true,
+		},
+		DiscordAuthorID: pgtype.Text{
+			String: request.DiscordAuthorId,
+			Valid:  true,
+		},
+		DiscordGuildID: pgtype.Text{
+			String: request.DiscordGuildId,
+			Valid:  true,
+		},
+		ChannelID: pgtype.Text{
+			String: request.ChannelId,
+			Valid:  true,
+		},
 	}
 
 	poll, err := p.qr.CreatePoll(ctx, createPollParams)
 	if err != nil {
-		return nil, err
+		return &polls.CreatePollResponse{}, err // Add transactional rollback
 	}
 
 	// Create new record for each option in polls_options table in DB
-	grpcOptions := make([]*polls.Option, 0, len(request.Options))
-	for _, option := range request.Options {
+	grpcOptions := make([]*polls.Option, 0, len(pollOptions))
+	for _, option := range pollOptions {
 		createOptionParams := queries.CreateOptionParams{}
 		createOptionParams.Title = pgtype.Text{
 			String: option,
@@ -51,12 +68,12 @@ func (p PollsServer) createPoll(ctx context.Context, request *polls.CreatePollRe
 
 		pollOption, err := p.qr.CreateOption(ctx, createOptionParams)
 		if err != nil {
-			return nil, err
+			return &polls.CreatePollResponse{}, err // Add transactional rollback
 		}
 
 		grpcPollOption := polls.Option{
 			Title:      pollOption.Title.String,
-			Id:         pollOption.ID,
+			CustomId:   fmt.Sprintf("poll_%d_choice_%d", poll.ID, pollOption.ID),
 			TotalVotes: 0,
 		}
 
