518777198920192faf6e7a0f2152dc34a702d549
maxguuse <max.pink.goose@gmail.com>
2024-02-20 20:20:44 +0300

    feat: added subcommands for listing and removing liveroles

--- apps/discord/internal/commands/liverole/cmd_listliveroles.go
+++ apps/discord/internal/commands/liverole/cmd_listliveroles.go
@@ -1,10 +1,38 @@
 package liverole
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/maxguuse/birdcord/apps/discord/internal/domain"
+	"github.com/samber/lo"
+)
 
 func (h *Handler) listLiveRoles(
 	i *discordgo.Interaction,
 	om optionsMap,
 ) (string, error) {
-	panic("not implemented")
+	ctx := context.Background()
+
+	guild, err := h.Database.Guilds().GetGuildByDiscordID(ctx, i.GuildID)
+	if err != nil {
+		return "", err
+	}
+
+	liveroles, err := h.Database.Liveroles().GetLiveroles(ctx, guild.ID)
+	if err != nil {
+		return "", err
+	}
+
+	if len(liveroles) == 0 {
+		return "Нет live-ролей.", nil
+	}
+
+	rolesList := lo.Map(liveroles, func(liverole *domain.Liverole, _ int) string {
+		return fmt.Sprintf("<@&%s>", liverole.DiscordRoleID)
+	})
+
+	return "Список live-ролей: \n" + strings.Join(rolesList, "\n"), nil
 }
--- apps/discord/internal/commands/liverole/cmd_removeliverole.go
+++ apps/discord/internal/commands/liverole/cmd_removeliverole.go
@@ -1,10 +1,35 @@
 package liverole
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"context"
+	"errors"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/maxguuse/birdcord/apps/discord/internal/domain"
+)
 
 func (h *Handler) removeLiveRole(
 	i *discordgo.Interaction,
 	om optionsMap,
 ) (string, error) {
-	panic("not implemented")
+	ctx := context.Background()
+
+	role := om["role"].RoleValue(h.Session, i.GuildID)
+
+	guild, err := h.Database.Guilds().GetGuildByDiscordID(ctx, i.GuildID)
+	if err != nil {
+		return "", errors.Join(domain.ErrInternal, err)
+	}
+
+	r, err := h.Database.Liveroles().GetLiverole(ctx, guild.ID, role.ID)
+	if err != nil {
+		return "", errors.Join(domain.ErrInternal, err)
+	}
+
+	err = h.Database.Liveroles().DeleteLiverole(ctx, r.ID)
+	if err != nil {
+		return "", errors.Join(domain.ErrInternal, err)
+	}
+
+	return "Live-роль успешно удалена.", nil
 }
